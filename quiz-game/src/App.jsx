b0eee5898418500a16e07299ad2fc90be912151f
Dawood Ahmad <dawood11@hotmail.com>
2023-09-16 01:24:34 +0200

    Quiz-game added useEffect

--- quiz-game/src/App.jsx
+++ quiz-game/src/App.jsx
@@ -1,95 +1,55 @@
-import './App.css';
+import "./App.css";
 
-import Button from './components/Button/Button';
-import { useState } from 'react';
+import { useEffect, useState } from "react";
+
+import Button from "./components/Button/Button";
+import quizList from "./quizList.json";
 
 const App = () => {
-  const [poeng, setPoeng] = useState(0);
-  // const [disableGame, setDisableGame] = useState(false);
-  // const [quizElementTeller, setQuizElementTeller] = useState(0);
-  let quizElementTeller = 0;
-  const quizList = [
-    {
-      spOrsmAl: "Spørsmål 1",
-      svarAlternativer: [
-        {
-        svar: "Svar verdi 1",
-        korrekt: false,
-        },
-        {
-          svar: "Svar verdi 2",
-          korrekt: false,
-        },
-        {
-          svar: "Svar verdi 3",
-          korrekt: true,
-        },
-        {
-          svar: "Svar verdi 4",
-          korrekt: false,
-        },
-      ]
-    },
-    {
-      spOrsmAl: "Spørsmål 2",
-      svarAlternativer: [
-        {
-        svar: "Svar2 verdi 1",
-        korrekt: false,
-        },
-        {
-          svar: "Svar2 verdi 2",
-          korrekt: false,
-        },
-        {
-          svar: "Svar2 verdi 3",
-          korrekt: true,
-        },
-        {
-          svar: "Svar2 verdi 4",
-          korrekt: false,
-        },
-      ]
-    },
-  ];
-  const [quiz, setQuiz] = useState(quizList[quizElementTeller]);
+	const [poeng, setPoeng] = useState(0);
+	const [quizElementTeller, setQuizElementTeller] = useState(0);
+	const [gameMode, setGameMode] = useState(false)
+	const [quiz, setQuiz] = useState(quizList[quizElementTeller]);
 
-  
+	useEffect(() => {
+		if (quizElementTeller === quizList.length) {
+			setGameMode(false);
+		}
+	}, [quizElementTeller]);
 
-  const setPoengFunksjon = (svarAlternativ) => {
-    console.log(svarAlternativ)
-    if (svarAlternativ.korrekt === true) {
-      setPoeng((prevState) => prevState + 1);
-    } else {
-      setPoeng((prevState) => prevState - 1);
-    }
+	const setPoengFunksjon = (svarAlternativ) => {
+		if (svarAlternativ.korrekt === true) {
+			setPoeng((prevState) => prevState + 1);
+		} else {
+			setPoeng((prevState) => prevState - 1);
+		}
 
-    quizElementTeller = quizElementTeller + 1;
-    // setQuizElementTeller((prevState) => prevState + 1);
-    // console.log(quizElementTeller, quizElementTeller === quizList.length)
-    // if (quizElementTeller === quizList.length) {
-    //   setDisableGame(true);
-    // }
-    setQuiz(quizList[quizElementTeller]);
-  };
+		setQuizElementTeller(quizElementTeller + 1);
+		setQuiz(quizList[quizElementTeller]);
+	};
 
-  return (
-    <div className="App">
-      {quizElementTeller}
-      <p>Poeng: {poeng}</p>
-      <h1>{quiz.spOrsmAl}</h1>
-        {
-          quiz.svarAlternativer.map((svarAlternativ, index) => (
-            <Button
-              key={index}
-              // disable={disableGame}
-              setPoengFunksjon={() => setPoengFunksjon(svarAlternativ)}
-              svarAlternativ={svarAlternativ}
-            />
-          ))
-        }
-    </div>
-  );
-}
+	return (
+		<div className="App">
+			<h1>Quiz game</h1>
+			{
+				gameMode === false
+				? <button onClick={() => setGameMode(true)}>Start spillet</button>
+				: <>
+					<p>Poeng: {poeng}</p>
+					<h1>{quiz.spOrsmAl}</h1>
+					{quiz.svarAlternativer.map(
+						(svarAlternativ, index) => (
+							<Button
+								key={index}
+								setPoengFunksjon={() => setPoengFunksjon(svarAlternativ)}
+								svarAlternativ={svarAlternativ}
+							/>
+						)
+					)}
+				</>
+			}
+		</div>
+	);
+};
 
 export default App;
--- quiz-game/src/components/Button/Button.jsx
+++ quiz-game/src/components/Button/Button.jsx
@@ -1,9 +1,12 @@
-const Button = ({svarAlternativ, setPoengFunksjon, disabled}) => {
-    return (
-        <button disabled={disabled} onClick={setPoengFunksjon} style={{ margin: '2%'}}>
-            {svarAlternativ.svar}
-        </button>
-    );
-}
+const Button = ({ svarAlternativ, setPoengFunksjon }) => {
+	return (
+		<button
+			onClick={setPoengFunksjon}
+			style={{ margin: "2%" }}
+		>
+			{svarAlternativ.svar}
+		</button>
+	);
+};
 
 export default Button;
